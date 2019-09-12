4e4d49e4386e37b71471e5bb6a3e8c092c699a1a
Ruan G. Ellis <ruan.ellis@alces-software.com>
2019-09-12 16:50:36 +0100

    Split apart

--- flight-gui.sh
+++ flight-gui.sh
@@ -21,7 +21,6 @@ yum -y install postgresql-server postgresql-devel
 
 postgresql-setup initdb
 sed -i 's/peer$/trust/g;s/ident$/trust/g' /var/lib/pgsql/data/pg_hba.conf
-systemctl start postgresql
 systemctl enable postgresql
 
 ## Install node.js 8.12.0
@@ -65,65 +64,3 @@ cp .env.example .env
 sed -i 's@^export APPLIANCE_INFORMATION_FILE_PATH=ABC@export APPLIANCE_INFORMATION_FILE_PATH=/appliance/cluster.md@g;s@^#RAILS_SERVE_STATIC_FILES@RAILS_SERVE_STATIC_FILES@g;s@^export SSH_KEYS_FILE_PATH=ABC@export SSH_KEYS_FILE_PATH=/appliance/siteadmin/.ssh/authorized_keys@g;s@export NETWORK_VARIABLES_FILE_PATH=ABC@export NETWORK_VARIABLES_FILE_PATH=/appliance/scripts/vars.sh@g;s@export NETWORK_SETUP_SCRIPT_FILE_PATH=ABC@export NETWORK_SETUP_SCRIPT_FILE_PATH=/appliance/scripts/personality_base.sh@g' .env
 touch /appliance/cluster.md
 
-bundle install
-
-## Generate database
-RAILS_ENV=production bin/rails db:create
-RAILS_ENV=production bin/rails db:schema:load
-RAILS_ENV=production bin/rails data:migrate
-
-## Create siteadmin Overware user
-echo "User.create(username: 'siteadmin', password: '$alces_SITEADMIN_PASS')" |RAILS_ENV=production rails console
-
-## Enable bolt-ons
-echo "bolt_on = BoltOn.find_by(name: 'VPN') ; bolt_on.enabled = true ; bolt_on.save! " |RAILS_ENV=production rails console
-echo "bolt_on = BoltOn.find_by(name: 'Console') ; bolt_on.enabled = true ; bolt_on.save! " |RAILS_ENV=production rails console
-echo "bolt_on = BoltOn.find_by(name: 'Assets') ; bolt_on.enabled = true ; bolt_on.save! " |RAILS_ENV=production rails console
-
-## Compile assets
-rake assets:precompile
-
-## Launch server in background
-cat << EOF > /usr/lib/systemd/system/flight-gui.service
-[Unit]
-Description=Alces Flight GUI Appliance
-Requires=network.target postgresql.service
-[Service]
-Type=simple
-User=root
-WorkingDirectory=/appliance/flight-gui
-ExecStart=/usr/bin/bash -lc 'bundle exec bin/rails server -e production --port 80'
-TimeoutSec=30
-RestartSec=15
-Restart=always
-[Install]
-WantedBy=multi-user.target
-EOF
-
-cat << EOF > /usr/lib/systemd/system/flight-terminal.service
-[Unit]
-Description=Flight terminal service
-Requires=network.target
-[Service]
-Type=simple
-User=root
-WorkingDirectory=/appliance/flight-terminal-service
-ExecStart=/usr/bin/bash -lc 'yarn run start'
-TimeoutSec=30
-RestartSec=15
-Restart=always
-[Install]
-WantedBy=multi-user.target
-EOF
-
-## Firewall service for Overware & Flight Terminal
-firewall-cmd --new-service flight-gui --permanent
-firewall-cmd --permanent --service=flight-gui --set-description="Flight management web interface" --add-port=25288/tcp --add-port=80/tcp --add-port=443/tcp
-firewall-cmd --add-service flight-gui --zone internal --permanent
-firewall-cmd --reload
-
-chmod 644 /usr/lib/systemd/system/overware.service /usr/lib/systemd/system/flight-terminal.service
-systemctl daemon-reload
-
-systemctl enable flight-gui.service
-systemctl enable flight-terminal.service
---
+++ flight-gui_postinstall.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+cd /appliance/flighthub-gui
+
+bundle install
+
+## Generate database
+RAILS_ENV=production bin/rails db:create
+RAILS_ENV=production bin/rails db:schema:load
+RAILS_ENV=production bin/rails data:migrate
+
+## Create siteadmin Overware user
+echo "User.create(username: 'siteadmin', password: '$alces_SITEADMIN_PASS')" |RAILS_ENV=production rails console
+
+## Enable bolt-ons
+echo "bolt_on = BoltOn.find_by(name: 'VPN') ; bolt_on.enabled = true ; bolt_on.save! " |RAILS_ENV=production rails console
+echo "bolt_on = BoltOn.find_by(name: 'Console') ; bolt_on.enabled = true ; bolt_on.save! " |RAILS_ENV=production rails console
+echo "bolt_on = BoltOn.find_by(name: 'Assets') ; bolt_on.enabled = true ; bolt_on.save! " |RAILS_ENV=production rails console
+
+## Compile assets
+rake assets:precompile
+
+## Launch server in background
+cat << EOF > /usr/lib/systemd/system/flight-gui.service
+[Unit]
+Description=Alces Flight GUI Appliance
+Requires=network.target postgresql.service
+[Service]
+Type=simple
+User=root
+WorkingDirectory=/appliance/flight-gui
+ExecStart=/usr/bin/bash -lc 'bundle exec bin/rails server -e production --port 80'
+TimeoutSec=30
+RestartSec=15
+Restart=always
+[Install]
+WantedBy=multi-user.target
+EOF
+
+cat << EOF > /usr/lib/systemd/system/flight-terminal.service
+[Unit]
+Description=Flight terminal service
+Requires=network.target
+[Service]
+Type=simple
+User=root
+WorkingDirectory=/appliance/flight-terminal-service
+ExecStart=/usr/bin/bash -lc 'yarn run start'
+TimeoutSec=30
+RestartSec=15
+Restart=always
+[Install]
+WantedBy=multi-user.target
+EOF
+
+## Firewall service for Overware & Flight Terminal
+firewall-cmd --new-service flight-gui --permanent
+firewall-cmd --permanent --service=flight-gui --set-description="Flight management web interface" --add-port=25288/tcp --add-port=80/tcp --add-port=443/tcp
+firewall-cmd --add-service flight-gui --zone internal --permanent
+firewall-cmd --reload
+
+
+chmod 644 /usr/lib/systemd/system/flight-gui.service /usr/lib/systemd/system/flight-terminal.service
+systemctl daemon-reload
+
+systemctl enable flight-gui.service
+systemctl enable flight-terminal.service
